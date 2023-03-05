686ddcdfb3eb974fd9536846a91f6c74e9f5526c
h3zord <lucaschmain@gmail.com>
2023-03-04 23:07:28 -0300

    Completing requirement 01

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_product = Product(
+        1,
+        'Alexa',
+        'Trybe',
+        '07/08/1995',
+        '07/08/2023',
+        '121519',
+        'Colocar na geladeira'
+    )
+
+    assert new_product.id == 1
+    assert new_product.nome_do_produto == 'Alexa'
+    assert new_product.nome_da_empresa == 'Trybe'
+    assert new_product.data_de_fabricacao == '07/08/1995'
+    assert new_product.data_de_validade == '07/08/2023'
+    assert new_product.numero_de_serie == '121519'
+    assert new_product.instrucoes_de_armazenamento == 'Colocar na geladeira'
