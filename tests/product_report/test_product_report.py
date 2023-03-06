63b3c684161274ed3e9fc52cbd8903900a92c0b4
h3zord <lucaschmain@gmail.com>
2023-03-06 20:40:14 -0300

    Completing requirement 08

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,21 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Water",
+        "Crystal",
+        "07/08/2022",
+        "07/08/2023",
+        "QWERTY16",
+        "na geladeira"
+    )
+
+    assert str(product.__repr__()) == (
+        "O produto Water"
+        " fabricado em 07/08/2022"
+        " por Crystal com validade"
+        " até 07/08/2023"
+        " precisa ser armazenado na geladeira."
+    )
