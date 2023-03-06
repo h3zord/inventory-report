229f7dad5d004e989ccdf78b1a01cf14e3b3b5c2
h3zord <lucaschmain@gmail.com>
2023-03-06 17:57:33 -0300

    Completing requirement 04 and 05

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,6 +2,7 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory:
@@ -9,7 +10,10 @@ class Inventory:
     def import_data(path, report_type):
         with open(path, encoding="utf-8") as file:
             products = csv.DictReader(file, delimiter=",", quotechar='"')
-            data = [product for product in products]
+            if (".csv" in path):
+                data = [product for product in products]
+            if (".json" in path):
+                data = json.load(file)
 
         if report_type == "simples":
             return SimpleReport.generate(data)
