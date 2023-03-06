3caac87b3472e6919c13d6483ed0e526d52b3122
h3zord <lucaschmain@gmail.com>
2023-03-06 20:18:21 -0300

    Fix requirement 04, 05 and 06

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,8 +1,8 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
-from importer.csv_importer import CsvImporter
-from importer.json_importer import JsonImporter
-from importer.xml_importer import XmlImporter
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
 
 
 class Inventory:
