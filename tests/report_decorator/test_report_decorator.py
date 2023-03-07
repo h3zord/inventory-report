ff2f49484fb309d0ef2555b1244fcd47abd78175
h3zord <lucaschmain@gmail.com>
2023-03-06 21:26:45 -0300

    Completing Requirement 09

--- tests/report_decorator/test_report_decorator.py
+++ tests/report_decorator/test_report_decorator.py
@@ -1,5 +1,23 @@
-# from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.colored_report import ColoredReport
+from inventory_report.reports.simple_report import SimpleReport
 
 
 def test_decorar_relatorio():
-    pass  # Seu teste deve ser escrito aqui
+    colored_report = ColoredReport(SimpleReport)
+
+    assert colored_report.generate([{
+        "id": "1",
+        "nome_do_produto": "Water",
+        "nome_da_empresa": "Crystal",
+        "data_de_fabricacao": "2022-08-07",
+        "data_de_validade": "2023-08-07",
+        "numero_de_serie": "QWERTY16",
+        "instrucoes_de_armazenamento": "na geladeira",
+    }]) == (
+        "\x1b[32mData de fabricação mais antiga:\x1b[0m "
+        "\x1b[36m2022-08-07\x1b[0m\n"
+        "\x1b[32mData de validade mais próxima:\x1b[0m "
+        "\x1b[36m2023-08-07\x1b[0m\n"
+        "\x1b[32mEmpresa com mais produtos:\x1b[0m "
+        "\x1b[31mCrystal\x1b[0m"
+    )
